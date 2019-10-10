7dcba6d8230fda2633a061c2d9f62d21cd32eed8
Elyic <fercordon87@gmail.com>
2019-10-09 23:40:20 -0600

    Conexion a Amazon....

---
+++ src/Modelo/Conexion.java
@@ -0,0 +1,28 @@
+
+package Modelo;
+
+import java.sql.*;
+
+public class Conexion 
+{
+  Connection con = null;
+
+ public Conexion() {
+  try {
+   Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+   con = DriverManager.getConnection("jdbc:sqlserver://ganasoft.cocqfacvxj99.us-east-2.rds.amazonaws.com:1433;databaseName=GANASOFT","admin","umg123456");
+   //jdbc:sqlserver://localhost:1433;databaseName=db_proyecto [sa on dbo]
+  
+  } 
+  catch (ClassNotFoundException | SQLException e) {
+   // TODO Auto-generated catch block
+   e.printStackTrace();
+  }
+ }
+ 
+ public Connection getConexion(){
+  return con;
+ }
+ 
+
+}
