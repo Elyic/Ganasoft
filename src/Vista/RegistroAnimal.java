0230f9592c2c61475a477423cedd980a1cb18551
MZXA <joseandresandy@gmail.com>
2019-11-01 10:35:13 -0600

    Generar reporte y qr realizados

--- src/Vista/RegistroAnimal.java
+++ src/Vista/RegistroAnimal.java
@@ -323,7 +323,8 @@ if (seleccion == fileChooser.APPROVE_OPTION)
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:        
+        // TODO add your handling code here:
+        
     }//GEN-LAST:event_jButton3ActionPerformed
     public void CargarRaza(){
      Modelo.MetodosRaza MR = new Modelo.MetodosRaza();
